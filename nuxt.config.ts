ca21f63416116d0005759f5f4a84498c956d8e8b
AshFoxMI <ashfox001@gmail.com>
2025-07-06 09:59:04 +0200

    go u fuq

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  srcDir: 'src',
+  ssr: false,
+  devServer: { host: process.env.TAURI_DEV_HOST || 'localhost' },
+
+  vite: {
+    clearScreen: false,
+    envPrefix: ['VITE_', 'TAURI_']
+  },
+
+  modules: ['@nuxt/ui'],
 })
